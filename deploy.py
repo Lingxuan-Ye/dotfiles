a26de498e1e90cd976c0d0ffa4b69573400006bd
Lingxuan Ye <love@lingxuan.io>
2023-10-28 18:48:08 +0800

    Modify message styling

--- deploy.py
+++ deploy.py
@@ -13,23 +13,41 @@ SOURCE = ROOT / 'source'
 BACKUPS = ROOT / 'backups'
 
 RESET = '\033[0m'
+BOLD = '\033[1m'
 RED = '\033[31m'
 GREEN = '\033[32m'
 YELLOW = '\033[33m'
 BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
 
-SUCCESS = GREEN + 'Success'.rjust(8) + RESET
-WARNING = YELLOW + 'Warning'.rjust(8) + RESET
-NOTICE = YELLOW + 'Notice'.rjust(8) + RESET
-ERROR = RED + 'Error'.rjust(8) + RESET
+
+def _padding(label: str) -> str:
+    return label.rjust(8) + ' '
+
+
+def success(message: str) -> None:
+    message = GREEN + BOLD + _padding('SUCCESS') + RESET + message
+    print(message)
+
+
+def info(message: str) -> None:
+    message = CYAN + BOLD + _padding('INFO') + RESET + message
+    print(message)
+
+
+def warning(message: str) -> None:
+    message = YELLOW + BOLD + _padding('WARNING') + RESET + message
+    print(message, file=sys.stderr)
+
+
+def error(message: str) -> None:
+    message = RED + BOLD + _padding('ERROR') + RESET + message
+    print(message, file=sys.stderr)
 
 
 def highlight_path(path: Path | str) -> str:
     return BLUE + str(path) + RESET
-
-
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
 
 
 def read_mapping(tree: dict) -> Generator[tuple[Path, Path], None, None]:
@@ -46,10 +64,7 @@ def read_mapping(tree: dict) -> Generator[tuple[Path, Path], None, None]:
                 source = Path(directory) / source
                 yield (source, destination)
         else:
-            eprint(
-                f'{ERROR}: found invalid scalar '
-                f'in {highlight_path("mapping.toml")}.'
-            )
+            error(f'found invalid scalar in {highlight_path("mapping.toml")}.')
 
 
 def backup(path: Path) -> None:
@@ -68,13 +83,13 @@ def main() -> None:
         destination.parent.mkdir(parents=True, exist_ok=True)
         if destination.is_dir() or destination.is_file():
             backup(destination)
-            print(
-                f'{NOTICE}: existing {highlight_path(destination)} '
-                f'has been backed up under {highlight_path(BACKUPS)}.'
+            info(
+                f'existing {highlight_path(destination)} has been '
+                f'backed up under {highlight_path(BACKUPS)}.'
             )
         elif destination.exists():
-            eprint(
-                f'{ERROR}: {highlight_path(source)} has been skipped, '
+            error(
+                f'{highlight_path(source)} has been skipped, '
                 f'as {highlight_path(destination)} does already exist '
                 'and is neither a directory nor a file, '
                 'nor a symbolic link pointing to them.'
@@ -82,20 +97,19 @@ def main() -> None:
             continue
         elif destination.is_symlink():
             destination.unlink()
-            eprint(
-                f'{WARNING}: found invalid symlink '
-                f'at {highlight_path(destination)}, '
+            warning(
+                f'found invalid symlink at {highlight_path(destination)}, '
                 'and now it has been removed.'
             )
 
         if (SOURCE / source).exists():
             destination.symlink_to(SOURCE / source)
-            print(
-                f'{SUCCESS}: {highlight_path(source)} has been '
+            success(
+                f'{highlight_path(source)} has been '
                 f'successfully deployed to {highlight_path(destination)}.'
             )
         else:
-            eprint(f'{ERROR}: {highlight_path(source)} does not exist.')
+            error(f'{highlight_path(source)} does not exist.')
 
 
 if __name__ == '__main__':
