d74e7257ae73990c67e05362621374ab277f3bf1
Lingxuan Ye <love@lingxuan.io>
2024-04-24 00:25:36 +0800

    Change string style to double quotes

--- deploy.py
+++ deploy.py
@@ -8,41 +8,41 @@ from time import time
 from typing import Generator
 
 ROOT = Path(__file__).parent
-CONFIG = ROOT / 'mapping.toml'
-SOURCE = ROOT / 'source'
-BACKUPS = ROOT / 'backups'
+CONFIG = ROOT / "mapping.toml"
+SOURCE = ROOT / "source"
+BACKUPS = ROOT / "backups"
 
-RESET = '\033[0m'
-BOLD = '\033[1m'
-RED = '\033[31m'
-GREEN = '\033[32m'
-YELLOW = '\033[33m'
-BLUE = '\033[34m'
-MAGENTA = '\033[35m'
-CYAN = '\033[36m'
+RESET = "\033[0m"
+BOLD = "\033[1m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
 
 
 def _padding(label: str) -> str:
-    return label.rjust(8) + ' '
+    return label.rjust(8) + " "
 
 
 def success(message: str) -> None:
-    message = GREEN + BOLD + _padding('SUCCESS') + RESET + message
+    message = GREEN + BOLD + _padding("SUCCESS") + RESET + message
     print(message)
 
 
 def info(message: str) -> None:
-    message = CYAN + BOLD + _padding('INFO') + RESET + message
+    message = CYAN + BOLD + _padding("INFO") + RESET + message
     print(message)
 
 
 def warning(message: str) -> None:
-    message = YELLOW + BOLD + _padding('WARNING') + RESET + message
+    message = YELLOW + BOLD + _padding("WARNING") + RESET + message
     print(message, file=sys.stderr)
 
 
 def error(message: str) -> None:
-    message = RED + BOLD + _padding('ERROR') + RESET + message
+    message = RED + BOLD + _padding("ERROR") + RESET + message
     print(message, file=sys.stderr)
 
 
@@ -54,9 +54,9 @@ def read_mapping(tree: dict) -> Generator[tuple[Path, Path], None, None]:
     for directory, value in tree.items():
         if isinstance(value, list):
             for record in value:
-                source = Path(directory) / record['filename']
+                source = Path(directory) / record["filename"]
                 destination = Path(
-                    expanduser(expandvars(record['destination']))
+                    expanduser(expandvars(record["destination"]))
                 )
                 yield (source, destination)
         elif isinstance(value, dict):
@@ -64,7 +64,7 @@ def read_mapping(tree: dict) -> Generator[tuple[Path, Path], None, None]:
                 source = Path(directory) / source
                 yield (source, destination)
         else:
-            error(f'found invalid scalar in {highlight_path("mapping.toml")}.')
+            error(f"found invalid scalar in {highlight_path('mapping.toml')}.")
 
 
 def backup(path: Path) -> None:
@@ -76,7 +76,7 @@ def backup(path: Path) -> None:
 
 def main() -> None:
 
-    with open(CONFIG, 'rb') as f:
+    with open(CONFIG, "rb") as f:
         config = tomllib.load(f)
 
     for source, destination in read_mapping(config):
@@ -84,33 +84,33 @@ def main() -> None:
         if destination.is_dir() or destination.is_file():
             backup(destination)
             info(
-                f'existing {highlight_path(destination)} has been '
-                f'backed up to {highlight_path(BACKUPS)}.'
+                f"existing {highlight_path(destination)} has been "
+                f"backed up to {highlight_path(BACKUPS)}."
             )
         elif destination.exists():
             error(
-                f'{highlight_path(source)} has been skipped, '
-                f'as {highlight_path(destination)} does already exist '
-                'and is neither a directory nor a file, '
-                'nor a symbolic link pointing to them.'
+                f"{highlight_path(source)} has been skipped, "
+                f"as {highlight_path(destination)} does already exist "
+                "and is neither a directory nor a file, "
+                "nor a symbolic link pointing to them."
             )
             continue
         elif destination.is_symlink():
             destination.unlink()
             warning(
-                f'found invalid symlink at {highlight_path(destination)}, '
-                'and now it has been removed.'
+                f"found invalid symlink at {highlight_path(destination)}, "
+                "and now it has been removed."
             )
 
         if (SOURCE / source).exists():
             destination.symlink_to(SOURCE / source)
             success(
-                f'{highlight_path(source)} has been '
-                f'successfully deployed to {highlight_path(destination)}.'
+                f"{highlight_path(source)} has been "
+                f"successfully deployed to {highlight_path(destination)}."
             )
         else:
-            error(f'{highlight_path(source)} does not exist.')
+            error(f"{highlight_path(source)} does not exist.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
