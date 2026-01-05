a5c2a010cb6398638208aa56603542f1bb3eb121
Lingxuan Ye <love@lingxuan.io>
2026-01-05 17:57:26 +0800

    Update config for `fish`

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,29 @@
-if test -f /opt/homebrew/bin/brew
-    eval (/opt/homebrew/bin/brew shellenv)
-end
+set fish_greeting ""
 
 fish_add_path --path /usr/local/sbin
 fish_add_path --path /usr/local/bin
+
+if type --query /opt/homebrew/bin/brew
+    /opt/homebrew/bin/brew shellenv | source
+end
 
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/bin
 
-set fish_greeting ""
-set -x EDITOR hx
+if type --query pacman
+    set --export EDITOR helix
+    abbr --add hx helix
+else
+    set --export EDITOR hx
+end
+
+if status is-interactive
+    starship init fish | source
+    zoxide init fish | source
+end
 
 abbr --add blog "$EDITOR ~/blog"
 abbr --add lab "$EDITOR ~/lab"
 abbr --add miri "cargo +nightly miri"
 abbr --add prune "git reflog expire --expire=now --all && git gc --prune=now"
-
-zoxide init fish | source
-
-if status is-interactive
-    starship init fish | source
-end
