a9bff5cb36a84f05f634384abf2f56edd8b81815
Lingxuan Ye <love@lingxuan.io>
2026-01-12 23:15:29 +0800

    Improve readability

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,3 @@
-set fish_greeting ""
-
 fish_add_path --path /usr/local/sbin
 fish_add_path --path /usr/local/bin
 
@@ -11,6 +9,12 @@ fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/bin
 
+if status is-interactive
+    set fish_greeting
+    starship init fish | source
+    zoxide init fish | source
+end
+
 if type --query pacman
     set --export EDITOR helix
     abbr --add hx helix
@@ -18,12 +22,7 @@ else
     set --export EDITOR hx
 end
 
-if status is-interactive
-    starship init fish | source
-    zoxide init fish | source
-end
-
-abbr --add blog "$EDITOR ~/blog"
-abbr --add lab "$EDITOR ~/lab"
+abbr --add blog "$EDITOR $HOME/blog"
+abbr --add lab "$EDITOR $HOME/lab"
 abbr --add miri "cargo +nightly miri"
 abbr --add prune "git reflog expire --expire=now --all && git gc --prune=now"
